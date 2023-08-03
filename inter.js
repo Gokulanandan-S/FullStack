6857190b3565377e87b8d3de3682365ea85d8b94
abc <anandhan04040@gmail.com>
2023-08-03 15:14:07 +0530

    nested loop

---
+++ inter.js
@@ -0,0 +1,31 @@
+const d=['gokul',19,'audi','harley',"I'm spiderman"]
+for (var ele in d){
+    console.log(d[ele])
+}
+console.log(d.length)
+a={
+    car:"Benz",
+    model:"V6",
+    airbags:10,
+    wheels:4,
+    color:"white"
+}
+console.log(a['car'])
+console.log(a.airbags)
+var some='model'
+console.log(a[some])
+console.log(a)
+a.wheels=10
+
+a.sunroof={ispresent:"yes"}
+console.log(a.sunroof.ispresent)
+for (var ele in a){
+    console.log(ele +" " + a[ele])
+    if (typeof a[ele] =="object")
+    {
+        for (var val in a[ele])
+        {
+            console.log(val +" "+a[ele][val])
+        }
+    }
+}
